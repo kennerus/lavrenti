6eaefea48daf846fa81d9e52a9efdbd190f45be5
Kennerus <kennerus@gmail.com>
2018-09-03 23:02:09 +0300

    wtf

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,15 @@
     <div class="row">
       <div class="col-6 offset-3">
         <div class="progress">
-          <div class="progress-bar" :style="progress"></div>
+          <div class="progress-bar" :style="progressWidth"></div>
         </div>
       </div>
 
       <form class="col-6 offset-3" action="/">
-        <div class="form-group">
-          <label for="name">Name: </label>
+        <div class="form-group" v-for="(field) in info">
+          <label :for="field.name">{{ field.name }}: </label>
           <i class="fas fa-check bg-success"></i>
-          <input type="text" class="form-control" id="name" v-model="info[0].value">
-        </div>
-
-        <div class="form-group">
-          <label for="phone">Phone: </label>
-          <i class="fas fa-check bg-success"></i>
-          <input type="text" class="form-control" id="phone" v-model="info[1].value">
-        </div>
-
-        <div class="form-group">
-          <label for="email">Email: </label>
-          <i class="fas fa-check"></i>
-          <input type="text" class="form-control" id="email" v-model="info[2].value">
-        </div>
-
-        <div class="form-group">
-          <label for="sf1">Some Field 1: </label>
-          <i class="fas fa-check bg-success"></i>
-          <input type="text" class="form-control" id="sf1" v-model="info[3].value">
-        </div>
-
-        <div class="form-group">
-          <label for="sf2">Some Field 2: </label>
-          <i class="fas fa-check bg-success"></i>
-          <input type="text" class="form-control" id="sf2" v-model="info[4].value">
+          <input type="text" class="form-control" :id="field.name" v-model="isValid">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
@@ -76,7 +52,25 @@ export default {
           value: '',
           pattern: /.+/
         }
-      ]
+      ],
+      progress: 0,
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+    progressWidth() {
+      return {
+        width: this.progress + '%'
+      };
+    }
+  },
+  watch: {
+    isValid(value, regExp) {
+      if (value.match(regExp)) {
+        this.progress =+ 20;
+      }
     }
   }
 }
