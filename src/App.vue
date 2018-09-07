441806b247fe9ca96b95f3325be31c59a761dc15
dmitryi <dmitryi.zavadskyi@yandex.ru>
2018-09-07 18:14:17 +0300

    # start second task

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,18 @@
       </div>
 
       <form class="col-6 offset-3" action="/">
-        <div class="form-group" v-for="(field) in info">
+        <div class="form-group"
+             v-for="(field) in info"
+             :key="field.name"
+        >
           <label :for="field.name">{{ field.name }}: </label>
-          <i class="fas fa-check bg-success"></i>
-          <input type="text" class="form-control" :id="field.name" v-model="isValid">
+          <i class="fas fa-check"></i>
+          <input type="text"
+                 class="form-control"
+                 :id="field.name"
+                 v-model="field.value"
+                 @input="isValid(field.value, field.pattern, field.progress)"
+          >
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
@@ -22,58 +30,80 @@
 
 <script>
 
-export default {
-  name: 'app',
-  data() {
-    return {
-      info: [
-        {
-          name: 'Name',
-          value: '',
-          pattern: /^[a-zA-z]{2,30}$/
-        },
-        {
-          name: 'Phone',
-          value: '',
-          pattern: /^[0-9]{7,14}$/
-        },
-        {
-          name: 'Email',
-          value: '',
-          pattern: /.+/
-        },
-        {
-          name: 'Some Field 1',
-          value: '',
-          pattern: /.+/
-        },
-        {
-          name: 'Some field 2',
-          value: '',
-          pattern: /.+/
-        }
-      ],
-      progress: 0,
-    }
-  },
-  methods: {
-
-  },
-  computed: {
-    progressWidth() {
+  export default {
+    name: 'app',
+    data() {
       return {
-        width: this.progress + '%'
-      };
-    }
-  },
-  watch: {
-    isValid(value, regExp) {
-      if (value.match(regExp)) {
-        this.progress =+ 20;
+        info: [
+          {
+            name: 'Name',
+            value: '',
+            pattern: /^[a-zA-z]{2,30}$/,
+            progress: false,
+          },
+          {
+            name: 'Phone',
+            value: '',
+            pattern: /^[0-9]{7,14}$/,
+            progress: false,
+          },
+          {
+            name: 'Email',
+            value: '',
+            pattern: /.+/,
+            progress: false,
+          },
+          {
+            name: 'Some Field 1',
+            value: '',
+            pattern: /.+/,
+            progress: false,
+          },
+          {
+            name: 'Some field 2',
+            value: '',
+            pattern: /.+/,
+            progress: false,
+          }
+        ],
+
+        successClass: ['fa-check', 'bg-success'],
+        errorClass: ['fa-times', 'bg-danger'],
+        defaultClass: 'hidden',
+        progress: 0,
       }
+    },
+    methods: {
+      isValid(value, pattern, progress) {
+        if (value.match(pattern) && !progress) {
+          progress = true;
+          this.progress += 20;
+        } else {
+          progress = false;
+          this.progress -= 20;
+        }
+      }
+    },
+    computed: {
+      progressWidth() {
+        return {
+          width: this.progress + '%'
+        };
+      },
+      // className() {
+      //   if () {
+      //     return this.successClass
+      //   } else if () {
+      //     return this.errorClass
+      //   } else {
+      //     return this.defaultClass
+      //   }
+      // }
+    },
+    watch: {
+
     }
   }
-}
 </script>
 
 <style scoped>
